f876d69ed06678a9b90a6b24100d45b8ed60f6cf
David Gilbert <dagilbert333@gmail.com>
2019-02-09 19:17:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-    sentences = self.split("." || "?" || "!").reject {|x| x.empty?}.count
+    sentences = self.split("." || "?" || "!").reject {|x| x.empty?}.size
   end
 end
